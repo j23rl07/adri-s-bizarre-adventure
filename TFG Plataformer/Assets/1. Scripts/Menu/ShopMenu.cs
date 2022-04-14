5a241230019f3e6aea5d9d2ab24cb1d6a15461ff
H3G0A <hegoaria@gmail.com>
2022-04-14 20:40:48 +0200

    #45 - feat: Mejoras de la interfaz Añadidas las dos pociones a la tienda y contador de monedas

--- TFG Plataformer/Assets/1. Scripts/Menu/ShopMenu.cs
+++ TFG Plataformer/Assets/1. Scripts/Menu/ShopMenu.cs
@@ -1,13 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ShopMenu : MonoBehaviour
 {
     [SerializeField] private GameObject hintText;
     [SerializeField] private GameObject shopCanvas;
+    [Header("Prices")]
+    [SerializeField] private int precioCafe;
+    [SerializeField] private int precioChuleta;
     
     private bool shopAccess = false;
+    private GameObject player;
+    private ScoreScript scoreScript;
 
 
     private void Start()
@@ -31,9 +37,10 @@ public class ShopMenu : MonoBehaviour
     {
         if (collision.gameObject.name.Equals("Player"))
         {
-
             hintText.gameObject.SetActive(true);
             shopAccess = true;
+            player = collision.gameObject;
+            scoreScript = player.GetComponent<ScoreScript>();
         }
     }
 
@@ -43,6 +50,7 @@ public class ShopMenu : MonoBehaviour
         {
             hintText.gameObject.SetActive(false);
             shopAccess = false;
+            player = null;
         }
     }
 
@@ -55,6 +63,9 @@ public class ShopMenu : MonoBehaviour
             PauseMenu.isGamePaused = true;
             Cursor.lockState = CursorLockMode.None;
             Cursor.visible = true;
+
+            //Visualizar el dinero del jugador
+            SetPrices();
         }
         else if(shopCanvas.activeSelf)
         {
@@ -64,6 +75,40 @@ public class ShopMenu : MonoBehaviour
             Cursor.lockState = CursorLockMode.Locked;
             Cursor.visible = false;
         }
+    }
 
+    private void SetPrices()
+    {
+        //Dinero del jugador
+        shopCanvas.transform.Find("Money/Text").gameObject.GetComponent<TextMeshProUGUI>().text = scoreScript.ScoreNum.ToString();
+        //Cafe
+        shopCanvas.transform.Find("HealthPotion/Text").gameObject.GetComponent<TextMeshProUGUI>().text = precioCafe.ToString();
+        //Chuleta
+        shopCanvas.transform.Find("ManaPotion/Text").gameObject.GetComponent<TextMeshProUGUI>().text = precioChuleta.ToString();
+    }
+
+    public void Buy(string obj)
+    {
+        int initialMoney = scoreScript.ScoreNum;
+
+        switch (obj)
+        {
+            case "Cafe":
+                if(initialMoney >= precioCafe)
+                {
+                    scoreScript.addMoney(-precioCafe);
+                    shopCanvas.transform.Find("Money/Text").gameObject.GetComponent<TextMeshProUGUI>().text = scoreScript.ScoreNum.ToString();
+                    player.GetComponent<PotionCountScript>().addPotion();
+                }
+                break;
+            case "Chuleta":
+                if (initialMoney >= precioChuleta)
+                {
+                    scoreScript.addMoney(-precioChuleta);
+                    shopCanvas.transform.Find("Money/Text").gameObject.GetComponent<TextMeshProUGUI>().text = scoreScript.ScoreNum.ToString();
+                    player.GetComponent<PotionCountScript>().addManaPotion();
+                }
+                break;
+        }
     }
 }

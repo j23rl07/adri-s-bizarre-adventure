72b82277288ebd3fbcdc6f4ed42078f6f986a156
Jaime Ramos Lara <j23rl07@gmail.com>
2022-06-07 08:23:52 +0200

    nivel redes estructura nivel

---
+++ TFG Plataformer/Assets/plattformRecorrido.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class plattformRecorrido : MonoBehaviour
+{
+    public float speed; //Velocidad de la plataforma
+    public int startingPoint; //Posicion de la plataforma (al comienzo)
+    public Transform[] points; //Posiciones de la plataforma
+
+    private int i; //indice
+
+    private GameObject player;
+
+    [SerializeField] private BoxCollider2D boxCollider;
+
+
+    void Start()
+    {
+        transform.position = points[startingPoint].position;
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector2.Distance(transform.position, points[i].position) < 0.02f)
+        {
+            i++;
+            if (i == points.Length || player.GetComponent<Player>().isDead)
+            {
+                transform.position = points[0].position;
+                this.gameObject.GetComponent<plattformRecorrido>().enabled = false;
+                boxCollider.enabled = true;
+            }
+        }
+
+        transform.position = Vector2.MoveTowards(transform.position, points[i].position, speed * Time.deltaTime); //posicion actual, posicion siguiente, velocidad
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            this.gameObject.GetComponent<plattformRecorrido>().enabled = true;
+            boxCollider.enabled = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        collision.transform.SetParent(transform);
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        collision.transform.SetParent(null);
+    }
+}
